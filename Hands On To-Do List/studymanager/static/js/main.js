434b7bbf1440d4d07818f507b169263c1e1bb795
davnucs <davnucs@gmail.com>
2025-11-24 17:49:26 +0800

    Delete Hands On To-Do List/studymanager/static/js directory

--- Hands On To-Do List/studymanager/static/js/main.js
+++
@@ -1,8 +0,0 @@
-function showAddBoardPopup() {
-  document.getElementById("addBoardPopup").style.display = "flex";
-}
-
-function closePopup() {
-  document.getElementById("addBoardPopup").style.display = "none";
-}
-
